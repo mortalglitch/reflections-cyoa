3748b70c777d89109a39657fa974444d039b68af
mortalglitch <mortal.glitch@gmail.com>
2025-11-06 14:19:38 -0500

    Updated main.py to include temporary working demo functions. These will be moved in the future.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 import pmtext.para
 import pmtext.util_pygame
+import random
+import math
 
 from config.default_config import SCREEN_HEIGHT, SCREEN_WIDTH
 
+game_frame = 0
+
 
 def main():
+    global game_frame
     # pygame init
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -17,7 +22,7 @@ def main():
 
     # pmtext init
     font = pmtext.util_pygame.TTF(
-        "./fonts/BigBlueTerminal/BigBlueTermPlusNerdFontMono-Regular.ttf", 12
+        "./fonts/BigBlueTerminal/BigBlueTermPlusNerdFontMono-Regular.ttf", 25
     )
     p = pmtext.para.Graph(font)
     t = pmtext.para.Typewriter(p)
@@ -25,11 +30,21 @@ def main():
     t.color(255, 255, 255)
     t.string("Hello ")
     t.newline()
-    t.wait(10)
+    t.wait(50)
     t.color(255, 0, 0)
     t.string("world")
     t.color(255, 255, 255)
     t.string(".")
+
+    jit = pmtext.para.Graph(font)
+    jit.shake(jitter)
+    jit.string("Hello... ")
+
+    jit.shake(singsong)
+    jit.string("World... ")
+
+    jit.shake(None)
+    jit.string("!! Back to your regular scheduled programming.")
 
     while True:
         for event in pygame.event.get():
@@ -42,11 +57,28 @@ def main():
         t.pulse()
         t.draw(screen, 10, 10)
 
+        jit.draw(screen, 10, 75)
+
         # Update the window
         pygame.display.flip()
+        game_frame += 1
 
         # print("Hello from reflections-cyoa!")
         dt = clock.tick(60) / 1000
+
+
+def jitter(char_index):
+    """Angry text motion."""
+    x = random.randint(0, 1)
+    y = random.randint(0, 1)
+    return x, y
+
+
+def singsong(char_index):
+    """Happy text motion."""
+    x = math.cos(float(game_frame + char_index) / 2)
+    y = math.sin(float(game_frame + char_index) / 2)
+    return x, y
 
 
 if __name__ == "__main__":
