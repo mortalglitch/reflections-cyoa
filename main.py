82d3b65a1b37cd2687d194d5b329a5d2091f2479
mortalglitch <mortal.glitch@gmail.com>
2025-11-06 12:11:08 -0500

    Added initial files and README placeholder

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+from config.default_config import *
+
+
+def main():
+    print("Hello from reflections-cyoa!")
+    print(SCREEN_HEIGHT)
+
+
+if __name__ == "__main__":
+    main()
